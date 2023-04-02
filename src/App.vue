7b9648aba299b936cda6694fdeadfa0afb44a319
a-3140 <intpblank@gmail.com>
2023-04-02 16:23:14 +0800

    chore: pulled a copy from my existing tailwind vue 3 with typescript template

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import DefaultLayout from "./layouts/DefaultLayout.vue";
+
+</script>
+
+<template>
+  <DefaultLayout>
+    <router-view />
+  </DefaultLayout>
+</template>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+
+</script>
+
+<template>
+    <div class="text-red-500">home</div>
+</template>
